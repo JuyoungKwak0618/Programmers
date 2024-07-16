271b7b6618fe8a316a24e94ec0b2c6ef9080d467
Juyoung Kwak <155405909+JuyoungKwak0618@users.noreply.github.com>
2024-07-16 14:51:51 +0900

    [level 0] Title: 아이스 아메리카노, Time: 0.02 ms, Memory: 76.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/120819. 아이스 아메리카노/아이스 아메리카노.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] solution(int money) {
+        // 초기값 설정
+        int[] answer = {0,0};
+        if(money >= 5500) {
+            answer[0] = money / 5500;
+            answer[1] = money - (5500 * answer[0]);
+        }
+            else {
+			 answer[0] = 0;
+			 answer[1] = money;
+		 }
+        return answer;
+    }
+}
